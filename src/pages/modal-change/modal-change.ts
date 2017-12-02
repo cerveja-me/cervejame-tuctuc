07ec18234a29fbe7d3de5a7216485bd228e7b6af
guardezi <guardezi@cerveja.me>
2017-12-01 23:30:24 -0200

    ajuste change modal

--- src/pages/modal-change/modal-change.ts
+++ src/pages/modal-change/modal-change.ts
@@ -13,31 +13,31 @@ export class ModalChangePage {
 
   ionViewDidLoad() {
     this.ord=this.navParams.get("order");
-    let value = this.ord.value;
+    let value = this.ord.payment_value;
     let nota=5;
     this.changes.push({
-      valor:(Math.floor(this.ord.value/nota)+1)*nota,
-      change:(Math.floor(this.ord.value/nota)+1)*nota-this.ord.value
+      valor:(Math.floor(this.ord.payment_value/nota)+1)*nota,
+      change:(Math.floor(this.ord.payment_value/nota)+1)*nota-this.ord.payment_value
     });
     nota=10;
     this.changes.push({
-      valor:(Math.floor(this.ord.value/nota)+1)*nota,
-      change:(Math.floor(this.ord.value/nota)+1)*nota-this.ord.value
+      valor:(Math.floor(this.ord.payment_value/nota)+1)*nota,
+      change:(Math.floor(this.ord.payment_value/nota)+1)*nota-this.ord.payment_value
     });
     nota=20;
     this.changes.push({
-      valor:(Math.floor(this.ord.value/nota)+1)*nota,
-      change:(Math.floor(this.ord.value/nota)+1)*nota-this.ord.value
+      valor:(Math.floor(this.ord.payment_value/nota)+1)*nota,
+      change:(Math.floor(this.ord.payment_value/nota)+1)*nota-this.ord.payment_value
     });
     nota=50;
     this.changes.push({
-      valor:(Math.floor(this.ord.value/nota)+1)*nota,
-      change:(Math.floor(this.ord.value/nota)+1)*nota-this.ord.value
+      valor:(Math.floor(this.ord.payment_value/nota)+1)*nota,
+      change:(Math.floor(this.ord.payment_value/nota)+1)*nota-this.ord.payment_value
     });
     nota=100;
     this.changes.push({
-      valor:(Math.floor(this.ord.value/nota)+1)*nota,
-      change:(Math.floor(this.ord.value/nota)+1)*nota-this.ord.value
+      valor:(Math.floor(this.ord.payment_value/nota)+1)*nota,
+      change:(Math.floor(this.ord.payment_value/nota)+1)*nota-this.ord.payment_value
     });
 
     console.log('ionViewDidLoad ModalChangePage', this.changes);
